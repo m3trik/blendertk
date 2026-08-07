1ca9cd80f3f5a64dd70620ad57a27cc165303d1b
m3trik@live.com <m3trik@live.com>
2026-08-07 01:12:13 -0400

    Bump version to 0.5.52 [skip ci]

--- blendertk/__init__.py
+++ blendertk/__init__.py
@@ -4,7 +4,7 @@ from pythontk.core_utils.module_resolver import bootstrap_package
 
 
 __package__ = "blendertk"
-__version__ = "0.5.51"
+__version__ = "0.5.52"
 
 """blendertk — Blender utilities that do for the tentacle Blender slots what mayatk does
 for the Maya slots.
